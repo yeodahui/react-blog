d62ed9bd459dbf3a3fc497fb02fc6fb96b2ce175
yeodahui <yeodahui.dev@gmail.com>
2022-06-17 22:47:41 +0900

    [컴포넌트 생성] Banner 컴포넌트 생성

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BannerContents from "./BannerContents/BannerContents";
+import "./banner.css";
+
+export default function Banner() {
+  return (
+    <div className="banner">
+      <div className="max-width">
+        <BannerContents />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Banner/BannerContents/BannerContents.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// TODO: post 넘어갔을 때 콘텐츠 바꾸기
+
+export default function BannerContents() {
+  return (
+    <div class="banner-contents">
+      <p class="sub-text">React Blog</p>
+      <p class="main-text">My BLOG</p>
+      <p class="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        nostrum dolor provident beatae deserunt magnam, illo neque. Maiores
+        ipsum commodi dolorum beatae aliquid laboriosam est
+      </p>
+    </div>
+  );
+}
