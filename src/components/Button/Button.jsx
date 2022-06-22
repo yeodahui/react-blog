1682ee27f4645710baec1d730b6d721f87ba318c
yeodahui <yeodahui.dev@gmail.com>
2022-06-23 08:39:47 +0900

    [컴포넌트 수정] 버튼 props와 class 수정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./button.css";
 
-export default function Button({iconURL, value, colorClass}) {
-  console.log(colorClass);
+export default function Button({iconURL, value, color}) {
+  const BUTTON = "button";
+  const LOWER_VALUE = value.toLowerCase();
+
   return (
-    <button type="button" className={`button ${colorClass}`}>
+    <button type="button" className={`${BUTTON} ${color} ${LOWER_VALUE}`}>
       <img src={iconURL} alt="" />
       <span>{value}</span>
     </button>
