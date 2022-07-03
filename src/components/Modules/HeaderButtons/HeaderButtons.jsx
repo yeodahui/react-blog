cb511780eb22c779ca72f5d34ea06536f3515df1
yeodahui <yeodahui.dev@gmail.com>
2022-07-03 14:57:53 +0900

    [컴포넌트 수정] 콘솔 출력 제거

--- src/components/Modules/HeaderButtons/HeaderButtons.jsx
+++ src/components/Modules/HeaderButtons/HeaderButtons.jsx
@@ -6,9 +6,6 @@ import iconLogout from "../../../assets/icon-logout.svg";
 import iconModifyWhite from "../../../assets/icon-modify-white.svg";
 
 export default function HeaderButtons({isLogined, loginHandler, profileImg}) {
-  console.log(isLogined)
-  console.log(loginHandler)
-  console.log(profileImg)
   return (
     <ul onClick={loginHandler}>
       {isLogined ? (
